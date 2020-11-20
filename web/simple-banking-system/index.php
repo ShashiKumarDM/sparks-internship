86e492d3086adfe2f882e94f416e8bcaf6fb7ec1
Shashi Kumar D M <dnksshashi@gmail.com>
2020-11-20 10:20:21 +0530

    final Commit

---
+++ web/simple-banking-system/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Shashi Kumar D M">
+    <meta name="copy rights" content="shashikumar@20">
+    <title>Home Page | Shashi Kumar D M</title>
+
+    <!-- styles -->
+    <link rel="stylesheet" href="./assets/css/main.min.css">
+
+</head>
+<body>
+
+    <?php include "./templets/header.html" ?>
+    
+    <main>
+        <a href="./allCustomers.php" class="view-btn">View all Customers</a>
+    </main>
+
+</body>
+</html>
